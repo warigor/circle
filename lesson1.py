71c136d28079f318a71f593c87f29eca606b289e
warigor <warigor228@gmail.com>
2019-02-12 19:54:13 +0200

    Message

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+# print("Enter your name ", end="")
+name = input("Enter your name ")
+age = int(input("Your age "))
+# print("Hello ", name, ".", sep='')
+print(f"Hello, {name}.")
+
+if age < 10:
+    print("Hi")
+elif 10 <= age <=30:
+    print("Hello")
+else:
+    print("Good day")
+
