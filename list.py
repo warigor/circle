060dfa791c088ad34666695de240a3650549d972
warigor <warigor228@gmail.com>
2019-02-14 20:44:19 +0200

    the third lesson lst edition

---
+++ list.py
@@ -0,0 +1,10 @@
+# ll = list()
+# ll = []     ОДне і теж
+ll = [134, 5, 53]
+print(len(ll))
+for i in range(len(ll)):
+    print(f'nomer {i}, ==>, {ll[i]}')
+    print()
+
+    for item in ll:
+        print(item)
